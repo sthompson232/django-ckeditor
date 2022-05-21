ea7bef2814282383bc51d24e11b7000ac51528f1
Sam Thompson <samthompson292@gmail.com>
2022-05-21 13:27:53 +0100

    updated path

--- ckeditor_uploader/urls.py
+++ ckeditor_uploader/urls.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import
 
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.decorators.cache import never_cache
 
 from . import views
 
-urlpatterns = [
-    url(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
-    url(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
-    url(r'^delete/', staff_member_required(views.delete), name='ckeditor_delete'),
-    url(r'^browseAllFiles/', never_cache(staff_member_required(views.browseAllFiles)), name='ckeditor_browseAllFiles'),
-    url(r'^browseImages/', never_cache(staff_member_required(views.browseImages)), name='ckeditor_browseImages'),
+re_pathpatterns = [
+    re_path(r'^upload/', staff_member_required(views.upload), name='ckeditor_upload'),
+    re_path(r'^browse/', never_cache(staff_member_required(views.browse)), name='ckeditor_browse'),
+    re_path(r'^delete/', staff_member_required(views.delete), name='ckeditor_delete'),
+    re_path(r'^browseAllFiles/', never_cache(staff_member_required(views.browseAllFiles)), name='ckeditor_browseAllFiles'),
+    re_path(r'^browseImages/', never_cache(staff_member_required(views.browseImages)), name='ckeditor_browseImages'),
 ]
